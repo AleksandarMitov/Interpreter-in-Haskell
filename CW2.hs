10fdf3e4bd2be7a89aa72f32cec56eb46e2c30d9
Alexander Mitov <alex.mitov96@gmail.com>
2017-04-07 19:15:33 +0300

    progress

--- CW2.hs
+++ CW2.hs
@@ -1,6 +1,13 @@
 module Main where
+import Control.Applicative
 import Prelude hiding (Num)
 import qualified Prelude (Num)
+import Control.Monad (void)
+import Data.List (intercalate)
+import Text.Megaparsec
+import Text.Megaparsec.Expr
+import Text.Megaparsec.String -- input stream is of type ‘String’
+import qualified Text.Megaparsec.Lexer as L
 
 type Num = Integer
 type Var = String
@@ -19,4 +26,9 @@ data Stm = Skip | Ass Var Aexp | Comp Stm Stm
         | If Bexp Stm Stm | While Bexp Stm
         | Block DecV DecP Stm | Call Pname
 
+testString = "/*fac_loop (p.23)*/\ny:=1;\nwhile !(x=1) do (\n y:=y*x;\n x:=x-1\n)"
+
 main = putStrLn "Hello, World!"
+
+parse :: String -> Stm
+parse (str) = Skip
